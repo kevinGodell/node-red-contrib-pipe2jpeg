0a98db5be8d579cb047409a1ca71a6f9a2e58be1
Kevin Godell <kevin.godell@gmail.com>
2020-08-23 12:17:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# node-red-contrib-pipe2jpeg
+######
+[![GitHub license](https://img.shields.io/badge/license-MIT-brightgreen.svg)](https://raw.githubusercontent.com/kevinGodell/node-red-contrib-pipe2jpeg/master/LICENSE?token=ABOPHYQ73XPHMEGBSABCDJK7IKRQO)
+[![npm](https://img.shields.io/npm/dt/node-red-contrib-pipe2jpeg.svg?style=flat-square)](https://www.npmjs.com/package/node-red-contrib-pipe2jpeg)
+[![GitHub issues](https://img.shields.io/github/issues/kevinGodell/node-red-contrib-pipe2jpeg.svg)](https://github.com/kevinGodell/node-red-contrib-pipe2jpeg/issues)
+#### What?
+- A node-red wrapper for pipe2jpeg.
+#### Why?
+- Needed for extracting individual jpeg(s) from a jpeg buffer stream.
+#### How?
+- Searches for the beginning (`0xFF 0x08`) and ending (`0xFF 0x09`) file markers.
+#### When?
+- Using ffmpeg to connect to a video source and piping out jpegs via `-c:v mjpeg -f image2pipe`.
+#### Requirements
+- Input must be a buffer stream that contains jpeg(s).
+- Output will be a buffer containing a single jpeg.
+#### Links
+- [node-red](https://nodered.org/)
+- [ffmpeg](https://ffmpeg.org/)
+- [buffer](https://nodejs.org/api/buffer.html)
+- [pipe2jpeg](https://www.npmjs.com/package/pipe2jpeg)
+#### Installation
+```
+npm install node-red-contrib-pipe2jpeg
+```
+#### Screenshots
+![screen shot 1](screenshot1.png)
---
+++ pipe2jpeg.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const P2J = require('pipe2jpeg');
+
+module.exports = function (RED) {
+  function Pipe2Jpeg(config) {
+    RED.nodes.createNode(this, config);
+
+    const p2j = new P2J();
+
+    const onJpeg = jpeg => {
+      this.status({ fill: 'green', shape: 'dot', text: 'jpeg ok' });
+
+      this.send({ payload: jpeg });
+    };
+
+    const onError = err => {
+      this.status({ fill: 'red', shape: 'dot', text: err });
+
+      this.error(`pipe2jpeg: ${err}`);
+    };
+
+    const onInput = msg => {
+      const { payload } = msg;
+
+      if (Buffer.isBuffer(payload) === true) {
+        return p2j.write(payload);
+      }
+
+      this.status({ fill: 'red', shape: 'dot', text: 'input error' });
+
+      this.error('pipe2jpeg: input must be a buffer');
+    };
+
+    const onClose = (removed, done) => {
+      p2j.resetCache();
+
+      if (removed) {
+        p2j.off('jpeg', onJpeg);
+
+        p2j.off('error', onError);
+
+        this.off('input', onInput);
+
+        this.off('close', onClose);
+
+        this.status({ fill: 'red', shape: 'ring', text: 'removed' });
+      } else {
+        this.status({ fill: 'red', shape: 'dot', text: 'closed' });
+      }
+
+      done && done();
+    };
+
+    p2j.on('jpeg', onJpeg);
+
+    p2j.on('error', onError);
+
+    this.on('input', onInput);
+
+    this.on('close', onClose);
+
+    this.status({ fill: 'green', shape: 'ring', text: 'initialized' });
+  }
+
+  RED.nodes.registerType('pipe2jpeg', Pipe2Jpeg);
+};
